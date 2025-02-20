1ef4d8a26b7815bde5aa2404493a25a54ff9d2b9
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-20 19:40:12 +0200

    still struggling to centre text

--- src/assets/components/hero/Hero.jsx
+++ src/assets/components/hero/Hero.jsx
@@ -3,7 +3,14 @@ import HeroCss from '../../css/Hero.module.css';
 const Hero = () => {
     return (
         <>
-        <div className={HeroCss.container}>
+        <div>
+            <div  className={HeroCss.img}><img src="src/assets/img/Image copy.png" alt="pizza" /></div>
+            <div className={HeroCss.text}>
+                <p>
+                Fuel your body & soul -
+                find recipes that taste amazing!
+                </p>
+            </div>
         </div>
         </>
     )
