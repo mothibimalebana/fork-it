bd92589019cee9b10092d294d55a1d8758a0bcb7
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-20 17:05:12 +0200

    made the image fit content of container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Hero from './assets/components/hero/Hero'
 import NavBar from './assets/components/navbar/NavBar'
 function App() {
   return(
     <>
     <NavBar/>
+    <Hero/>
     </>
   )
 }
---
+++ src/assets/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import HeroCss from '../../css/Hero.module.css';
+
+const Hero = () => {
+    return (
+        <>Image
+        <div className={Hero.container}>
+            <img src="src/assets/img/Image.png"/>
+        </div>
+        </>
+    )
+}
+
+export default Hero
