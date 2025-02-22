075c227020f36f4577fa9e45c3bbc459f4fb477a
mothibimalebana <mothibi.mm12@gmail.com>
2025-02-22 16:14:29 +0200

    created fetch to get api end points in Food.jsx

---
+++ src/assets/api/Food.jsx
@@ -0,0 +1,13 @@
+const fetchFood = (food) =>{
+    fetch(`www.themealdb.com/api/json/v1/1/search.php?s=${food}`)
+}
+
+
+const Food = () => {
+    const [foodList, setFoodList] = [];
+
+    return(
+        <>
+        </>
+    )
+}
