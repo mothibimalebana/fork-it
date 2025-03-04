cc77d173dbe585fc25430abc888397a3b05a9389
mothibimalebana <mothibi.mm12@gmail.com>
2025-03-04 14:30:46 +0200

    images and title are now appearing on the screen

--- src/assets/components/content/Content.jsx
+++ src/assets/components/content/Content.jsx
@@ -1,21 +1,49 @@
-import Food from "../../api/Food";
+import { useState ,useEffect } from "react";
+
 
 const Content = () => {
+    const [foodList, setFoodList] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken');
+                const data = await response.json();
+                const foods = await data.meals;
+            
+                setFoodList(foods)                
+            }
+            catch(error){
+                console.error(error.message);
+            }
+        }
+        fetchData();
+    },[])
 
+    const CardItems = ({title, imgSrc, }) =>{
+        console.log(title, imgSrc)
+
+        return(
+            <>
+            <div className="cardItem">
+                <img src={imgSrc} alt={title}/>
+                <h4>{title}</h4>
+            </div>
+            </>
+        )
+    }
+
+    console.log(foodList)
     return(
         <>
         <div>
             <section>
-                <h3>Effortless Eats</h3>
-            </section>
-            <section>
-                <h3>Healthy Eating inspiration</h3>
-            </section>
-            <section>
-                <h3>Liquid Harmony</h3>
-            </section>
-            <section>
-                <h3>Recipe of the week</h3>
+                <h3>Chicken Recipes</h3>
+                <div className="cards">
+                {foodList.map((foodItem) => {
+                    return <CardItems key={foodItem.idMeal} title={foodItem.strMeal} imgSrc={foodItem.strMealThumb}/>
+                })}
+                </div>
             </section>
         </div>
         </>
