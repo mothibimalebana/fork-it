6d5913ca4184e834387cacb5689756c16869150e
mothibimalebana <mothibi.mm12@gmail.com>
2025-03-12 21:53:28 +0200

    changed navbar options to: chicken, ribs, beef
    next time i am going to use react router to switch between all these pages

--- src/assets/components/navbar/NavBar.jsx
+++ src/assets/components/navbar/NavBar.jsx
@@ -33,9 +33,9 @@ const NavBar = () => {
                 </svg>
                 <h2 className={NavBarCss.logo}>Fork <br />It</h2>
                 <div className={NavBarCss.left}>
-                <NavButtons label='RECIPES'/>
-                <NavButtons label='ARTICLES'/>
-                <NavButtons label='ABOUT'/>
+                <NavButtons label='Chicken'/>
+                <NavButtons label='Beef'/>
+                <NavButtons label='Ribs'/>
                 </div>
             </div>
             <NavItems/>
